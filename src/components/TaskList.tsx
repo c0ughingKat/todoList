163718d93b8164a01398f7dab82b97b7c5527299
K4t <c0ughingKat@protonmail.com>
2022-01-06 20:07:01 -0800

    :tada: Challenge finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/tasklist.scss'
 
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle) return;
+
+    const newTask = [
+      {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+      }
+    ]
+    const listTasks = tasks.concat(newTask)
+    setTasks(listTasks);
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    const toggleTaskList = tasks.map(task => task.id == id ? {...task, isComplete: !task.isComplete} : {...task})
+    setTasks(toggleTaskList)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const listTask = tasks.filter(task => task.id !== id)
+
+    setTasks(listTask)
   }
 
   return (
